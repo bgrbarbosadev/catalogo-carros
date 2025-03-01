23d5a53b9b6a6eb0addb515af3b3c805fd469dea
bgrbarbosadev <bgrbarbosa.dev@gmail.com>
2025-02-28 21:08:25 -0300

    Projeto Catalogo-CarrosAPI clean criado

---
+++ catalogo-carros-api/src/main/java/br/com/bgrbarbosa/catalogo_carros_api/CatalogoCarrosApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.bgrbarbosa.catalogo_carros_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogoCarrosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogoCarrosApiApplication.class, args);
+	}
+
+}
---
+++ catalogo-carros-api/src/test/java/br/com/bgrbarbosa/catalogo_carros_api/CatalogoCarrosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bgrbarbosa.catalogo_carros_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogoCarrosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
